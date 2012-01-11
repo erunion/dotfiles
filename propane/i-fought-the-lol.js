5d8cff5cf7ea06def25899168b53a4f71600bd7e
Jon Ursenbach <jon@ursenba.ch>
2012-01-11 11:53:00 -0800

    i fought the lol v0.9

--- propane/i-fought-the-lol.js
+++ propane/i-fought-the-lol.js
@@ -1,15 +1,11 @@
 /**
  * I Fought The LOL, the Campfire "enhancer"
- * v0.8b - June 14, 2011
+ * v0.9a - January 11, 2012
  * Aaron Draczynski
  * ================================================
  * This file goes into your /Users/youraccount/Library/Application Support/Propane/unsupported/ folder
  * Restart Propane after installing or removing this script
  * ================================================
- * What's new in v0.8b?
- *   Replay button for sounds
- *   External sound override is now working properly ("/sound -u http://mydomain.com/mysound.mp3")
- *   Enhanced clip playback controls including fullscreen mode
  */
 
 Campfire.IFoughtTheLOL = Class.create({
@@ -26,7 +22,7 @@ Campfire.IFoughtTheLOL = Class.create({
           u = false,
           r = v[0],
           x = v[2],
-          c = v[5];
+          c = v[6];
       if (e == '/sound ' && z == true) {
         var s = t.substring(7,t.length).replace(/^\s\s*/, '').replace(/\s\s*$/, ''),
             fs = '"<strong>' + s + '</strong>"';
@@ -54,10 +50,7 @@ Campfire.IFoughtTheLOL = Class.create({
           b.innerHTML = c + '#d5312b"><em>Why are you closed?!?</em></span>';
         }, true);
         f.addEventListener('error', function(e) {
-          b.innerHTML = c + '#d5312b"><em>You dun goofed,  ' + fs + ' could not be played.</em> [' + f.error.code + ']</span>';
-        }, true);
-        f.addEventListener('stalled', function(e) {
-          b.innerHTML = c + '#d5312b"><em>The audio player stalled while buffering ' + fs + ' &mdash; try the <a href="' + r + s + x + '" target="_blank" style="color:#d5312b">direct link</a>.</em></span>';
+          b.innerHTML = c + '#d5312b"><em>' + fs + ' isn\'t a valid sound.</em></span>';
         }, true);
         f.addEventListener('suspend', function(e) {
           b.innerHTML = c + '#d5312b"><em>Consequences will never be the same.</em></span>';
@@ -100,9 +93,9 @@ Campfire.IFoughtTheLOL = Class.create({
         }
       }
       if (m == '/clip' && z == true) {
-        var l = b.querySelector('a');
+        var l = b.querySelector('a'),
             s = l.getAttribute('href'),
-            f = document.createElement(v[4]);
+            f = document.createElement(v[4]),
             msg = document.createElement('div');
         msg.style.padding = '6px 0 3px';
         f.style.margin = '0 0 3px';
@@ -140,7 +133,109 @@ Campfire.IFoughtTheLOL = Class.create({
         f.play();
         window.scrollTo(0, document.body.scrollHeight);
       } else if (m == '/clip' && z == false) {
-        b.innerHTML = c + '#888"><em>Played a clip. If you turned off <strong>/mute</strong>, you would have seen it.</em></span>';
+        b.innerHTML = c + '#888"><em>Played a clip. If you turned off <strong>/mute</strong>, you would\'ve seen it.</em></span>';
+      }
+      if (e == '/combo ' && z == true) {
+        var l = b.querySelector('a'),
+            s = l.getAttribute('href'),
+            f = document.createElement(v[5]),
+            g = document.createElement(v[5]),
+            j = document.createElement(v[3]),
+            msg = document.createElement('div'),
+            k = t.substr(t.lastIndexOf(' ') + 1),
+            dl = 500;
+        if (t.indexOf('|') != -1) {
+          k = t.substring(t.lastIndexOf(' ') + 1, t.indexOf('|'));
+          dl = parseInt(t.substr(t.indexOf('|') + 1)) * 1000;
+          if (dl > 10000) {
+            dl = 10000;
+          }
+        }
+        f.style.width = 0;
+        f.style.height = 0;
+        f.style.display = 'none';
+        f.src = s;
+        g.style.height = '90px';
+        g.src = r + 'r.gif';
+        b.innerHTML = '';
+        b.appendChild(f, b.firstChild);
+        b.appendChild(g, b.firstChild);
+        b.appendChild(msg);
+        j.addEventListener('canplay', function() {
+          j.pause();
+          j.currentTime = 0;
+        }, true);
+        j.addEventListener('canplaythrough', function() {
+          j.pause();
+          j.currentTime = 0;
+          msg.setAttribute('data-sound-ready', 1);
+          if (msg.hasAttribute('data-image-ready')) {
+            msg.style.padding = '6px 0 3px';
+            g.style.height = 'auto';
+            g.style.margin = '0 0 3px';
+            g.src = f.src;
+            b.removeChild(f);
+            window.scrollTo(0, document.body.scrollHeight);
+            setTimeout(function() {
+              j.play();
+              window.scrollTo(0, document.body.scrollHeight);
+            }, dl);
+            msg.innerHTML = c + '#4f9707"><em>Playing combo...</em></span>';
+          }
+        }, true);
+        j.addEventListener('empty', function(e) {
+          msg.innerHTML = c + '#d5312b"><em>Why are you closed?!?</em></span>';
+        }, true);
+        j.addEventListener('error', function(e) {
+          msg.style.padding = '6px 0 3px';
+          g.src = r + 'f.gif';
+          msg.innerHTML = c + '#d5312b"><em>"<strong>' + k + '</strong>" isn\'t a valid sound.</em></span>';
+        }, true);
+        j.addEventListener('suspend', function(e) {
+          msg.innerHTML = c + '#d5312b"><em>Consequences will never be the same.</em></span>';
+        }, true);
+        j.addEventListener('dataunavailable', function(e) {
+          msg.innerHTML = c + '#d5312b"><em>Not my chair, not my problem.</em></span>';
+        }, true);
+        j.addEventListener('ended', function() {
+          msg.innerHTML = c + '#888"><em>Played "<strong>' + k + '</strong>" combo. <a class="replay">Replay</a> or <a class="combo_breaker">collapse from view</a>?</a></em></span>';
+          var br = b.querySelector('.replay'),
+            cr = b.querySelector('.combo_breaker');
+          br.addEventListener('click', function() {
+            j.currentTime = 0;
+            j.play();
+          }, false);
+          cr.addEventListener('click', function() {
+            g.parentNode.innerHTML = c + '#888"><em>Combo removed.</em></span>';
+            b.removeChild(f);
+            msg.style.padding = '2px 0 1px';
+          });
+          j.pause();
+          j.currentTime = 0;
+          j.src = '';
+          var pn = j.parentNode;
+          pn.removeChild(j);
+        }, true);
+        j.src = r + k + x;
+        f.addEventListener('load', function(e) {
+          msg.setAttribute('data-image-ready', 1);
+          if (msg.hasAttribute('data-sound-ready')) {
+            msg.style.padding = '6px 0 3px';
+            g.style.height = 'auto';
+            g.style.margin = '0 0 3px';
+            g.src = f.src;
+            b.removeChild(f);
+            window.scrollTo(0, document.body.scrollHeight);
+            setTimeout(function() {
+              j.play();
+              window.scrollTo(0, document.body.scrollHeight);
+            }, dl);
+            msg.innerHTML = c + '#4f9707"><em>Playing combo...</em></span>';
+          }
+        }, false);
+        window.scrollTo(0, document.body.scrollHeight);
+      } else if (e == '/combo ' && z == false) {
+        b.innerHTML = c + '#888"><em>Played a combo. If you turned off <strong>/mute</strong>, you would\'ve seen it.</em></span>';
       }
       if (m == '/mute') {
         if (Element.hasClassName(q.element, 'you')) {
@@ -152,7 +247,7 @@ Campfire.IFoughtTheLOL = Class.create({
             b.innerHTML = c + '#4b49bc"><em>You just turned mute <strong>off</strong>.</em></span>';
           }
         } else {
-          b.innerHTML = c + '#4b49bc"><em>Just toggled their mute setting.</em></span>';
+          b.innerHTML = c + '#4b49bc"><em>=Just toggled their mute setting.</em></span>';
         }
       }
     }
@@ -170,6 +265,6 @@ Campfire.IFoughtTheLOL = Class.create({
 });
 
 var z = true,
-    v = ['http://papermodelplane.com/box/sounds/', 'http://www.papermodelplane.com/box/achievement_', '.mp3', 'audio', 'video', '<span style="color:'];
+    v = ['http://papermodelplane.com/box/sounds/', 'http://www.papermodelplane.com/box/achievement_', '.mp3', 'audio', 'video', 'img', '<span style="color:'];
 Campfire.Responders.push('IFoughtTheLOL');
 window.chat.installPropaneResponder('IFoughtTheLOL', 'andTheLOLWon');
